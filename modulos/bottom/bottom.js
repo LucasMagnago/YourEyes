e16cd683a8b83303e3542525e6133219563b5e41
Lucas Magnago de Oliveira <you@example.com>
2021-11-24 15:45:46 -0300

    it's working

--- modulos/bottom/bottom.js
+++ modulos/bottom/bottom.js
@@ -1,20 +1,36 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableHighlight, Image , Vibration} from 'react-native';
 import style from './style';
+import Sound from 'react-native-sound';
 
 export default function Bottom(){
+
+    const sound = new Sound('beep.mp3');
+    const ONE_SECOND_IN_MS = 1000;
 
     const [vibrar, setVibrar] = useState(true)
     const [audio, setAudio] = useState(true)
     const [bluetooth, setBluetooth] = useState(true)
 
     function StateChange(state){
-        const ONE_SECOND_IN_MS = 1000;
+
         const PATTERN = [0.1 * ONE_SECOND_IN_MS, 0.125 * ONE_SECOND_IN_MS,0.1 * ONE_SECOND_IN_MS, 0.125 * ONE_SECOND_IN_MS]
+        
         if(state){
             Vibration.vibrate(0.15 * ONE_SECOND_IN_MS);
         }else{
             Vibration.vibrate(PATTERN);
+        }
+    }
+
+    function ExecutarAudio(state){
+        if(state){
+            sound.play()
+        }
+    }
+    function ExecutarVibracao(state){
+        if(state){
+            Vibration.vibrate(0.1 * ONE_SECOND_IN_MS)
         }
     }
     
